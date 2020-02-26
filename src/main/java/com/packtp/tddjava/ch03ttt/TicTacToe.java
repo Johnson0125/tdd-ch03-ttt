e2132f1e4c679327c49793592e93a387d7ac1446
Johnson <superzsen@126.com>
2020-02-26 22:22:45 +0800

    check asix and set box position

---
+++ src/main/java/com/packtp/tddjava/ch03ttt/TicTacToe.java
@@ -0,0 +1,31 @@
+package com.packtp.tddjava.ch03ttt;
+
+/**
+ * @author Johnson
+ * @date 2020/2/26 22:05
+ */
+public class TicTacToe {
+    private Character[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
+
+    public void play(int x, int y) {
+        checkAxis(x);
+        checkAxis(y);
+
+        setBox(x, y);
+
+    }
+
+    private void setBox(int x, int y) {
+        if (board[x - 1][y - 1] != '\0') {
+            throw new RuntimeException(" box is occupied");
+        } else {
+            board[x - 1][y - 1] = 'X';
+        }
+    }
+
+    private void checkAxis(int axis) {
+        if (axis > 3 || axis < 1) {
+            throw new RuntimeException("X is outside board");
+        }
+    }
+}
---
+++ src/test/java/com/packtp/tddjava/ch03ttt/TicTacToeSpec.java
@@ -0,0 +1,41 @@
+package com.packtp.tddjava.ch03ttt;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @author Johnson
+ * @date 2020/2/26 17:46
+ */
+public class TicTacToeSpec {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private TicTacToe ticTacToe;
+
+    @Before
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
+
+    @Test
+    public void whenYOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2, 5);
+    }
+
+    @Test
+    public void whenOccupiedThenRuntimeException() {
+        ticTacToe.play(2, 1);
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2, 1);
+    }
+}
